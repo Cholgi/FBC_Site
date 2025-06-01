2445e9d978d334276d6e8b3f32405ddeca12aac9
cholgi <rladidcjf0101@gmail.com>
2025-06-02 00:21:20 +0900

    1try

---
+++ update_results.py
@@ -0,0 +1,14 @@
+result = "테스트문구예용~"
+
+import json
+import os
+os.makedirs("web/data", exist_ok=True)
+
+with open("web/data/results.json", "a", encoding="utf-8") as f:
+    f.write(result + "\n")  # 줄바꿈 포함해서 누적 저장
+
+
+os.system("git add web/data/news_results.json")
+os.system('git commit -m "update result"')
+os.system("git push origin main")  # 브랜치 이름에 따라 'main' or 'master'
+
